2c4d243e812406c6bbe3ed3cbf4db61f8d77c3ed
Worm-Hole <87369606+Worm-Hole@users.noreply.github.com>
2026-01-29 23:27:04 +0900

    Compat for This Is Mine

---
+++ Source/Mods/ThisIsMine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using HarmonyLib;
+using Verse;
+
+namespace Multiplayer.Compat.Mods
+{
+    [MpCompatFor("Mlie.ThisIsMine")]
+    public class ThisIsMine
+    {
+        private static Type CompBelongType;
+
+        public ThisIsMine(ModContentPack mod)
+        {
+            CompBelongType = AccessTools.TypeByName("ThisIsMine.CompCanBelongToRoomOwners");
+
+            MpCompat.RegisterLambdaMethod(CompBelongType, "CompGetGizmosExtra", 0, 1, 2, 3);
+        }
+    }
+}
