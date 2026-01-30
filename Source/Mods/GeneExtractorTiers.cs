e36f1c6187b081e3a80feb661b7c87f2479e361e
Worm-Hole <87369606+Worm-Hole@users.noreply.github.com>
2026-01-30 13:43:13 +0900

    compat for GeneExtractorTiers (Not Tested)

---
+++ Source/Mods/GeneExtractorTiers.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+
+    [MpCompatFor("RedMattis.GeneExtractor")]
+    public class GeneExtractorTiers
+    {
+        private static Type GeneExtractorTier;
+
+        public GeneExtractorTiers(ModContentPack mod)
+        {
+            LongEventHandler.ExecuteWhenFinished(LatePatch);
+            GeneExtractorTier = AccessTools.TypeByName("GeneExtractorTiers.Building_GeneExtractorTier");
+        }
+        private static void LatePatch()
+        {
+            MpCompat.RegisterLambdaMethod(GeneExtractorTier, "GetFloatMenuOptions", 0, 1);
+            MpCompat.RegisterLambdaMethod(GeneExtractorTier, "GetGizmos", 0, 1, 2, 3, 4, 5, 6);
+        }
+    }
+}
