4a528cf24863d1764ceebcff6f3acdbb2190722e
Worm-Hole <87369606+Worm-Hole@users.noreply.github.com>
2026-01-30 13:54:44 +0900

    Initial compat for Dimensions Repocketed (Not tested)

---
+++ Source/Mods/Dimensions.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Multiplayer.Compat.Mods
+{
+    [MpCompatFor("KB.PocketDimension")]
+    public class Dimensions
+    {
+        private static Type PocketDimensionBox;
+
+        public Dimensions(ModContentPack mod)
+        {
+            LongEventHandler.ExecuteWhenFinished(LatePatch);
+            PocketDimensionBox = AccessTools.TypeByName("KB_PocketDimension.Building_PocketDimensionBox");
+        }
+        private static void LatePatch()
+        {
+            MpCompat.RegisterLambdaMethod(PocketDimensionBox, "GetGizmos", 0, 1, 2, 3);
+        }
+    }
+}
