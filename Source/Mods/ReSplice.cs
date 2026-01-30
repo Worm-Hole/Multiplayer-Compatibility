9d1cda357e4d4378e323b638ee4f48ee85ea934d
Worm-Hole <87369606+Worm-Hole@users.noreply.github.com>
2026-01-30 13:50:13 +0900

    Initial compat for Resplice (only gizmos are synced)

---
+++ Source/Mods/ReSplice.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Multiplayer.Compat.Mods
+{
+    [MpCompatFor("ReSplice.XOTR.Core")]
+    public class ReSplice
+    {
+        private static Type GeneCentrifuge;
+        private static Type XenogermDuplicator;
+        private static Type ArchiteRefuelableType;
+        private static Type DarkArchiteSpikeType;
+
+        public ReSplice(ModContentPack mod)
+        {
+            LongEventHandler.ExecuteWhenFinished(LatePatch);
+            GeneCentrifuge = AccessTools.TypeByName("ReSpliceCore.Building_GeneCentrifuge");
+            XenogermDuplicator = AccessTools.TypeByName("ReSpliceCore.Building_XenogermDuplicator");
+            ArchiteRefuelableType = AccessTools.TypeByName("ReSpliceCore.CompArchiteRefuelable");
+            DarkArchiteSpikeType = AccessTools.TypeByName("ReSpliceCore.DarkArchiteSpike");
+        }
+        private static void LatePatch()
+        {
+            MpCompat.RegisterLambdaMethod(GeneCentrifuge, "GetGizmos", 0, 1, 2, 3);
+            MpCompat.RegisterLambdaMethod(XenogermDuplicator, "GetGizmos", 0, 1, 2, 3, 4);
+            MpCompat.RegisterLambdaMethod(ArchiteRefuelableType, "CompGetGizmosExtra", 0);
+            MpCompat.RegisterLambdaMethod(DarkArchiteSpikeType, "GetFloatMenuOptions", 0);
+            MpCompat.RegisterLambdaMethod(DarkArchiteSpikeType, "GetGizmos", 0, 1, 2, 3);
+        }
+    }
+}
